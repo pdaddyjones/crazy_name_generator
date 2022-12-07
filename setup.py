e230d12b0278b618ecdf1148f3aa0b4ec89b3837
pdaddyjones <119063171+pdaddyjones@users.noreply.github.com>
2022-12-07 14:54:58 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,44 @@
-from distutils.core import setup
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
 
 setup(
-    version="1.0.0",
     name="crazy_name_generator",
-    description="A simple crazy name generator",
+    version="0.1.0",
+    url="",
+    license='GPLv3',
+
+    author="Pi Daddy Jones III",
+    author_email="pdaddyjonesthethird@gmail.com",
+
+    description="A crazy name generator",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=['anticaptchaofficial'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
